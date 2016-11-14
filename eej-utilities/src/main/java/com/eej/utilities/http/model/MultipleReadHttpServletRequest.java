35ccd55f3b856ed23b535be3e8ec0bdc297d8cb3
Mikel Ibiricu <jlumietu@gmail.com>
2016-11-14 16:20:41 +0100

    MultipleReadHttpServletRequest

---
+++ eej-utilities/src/main/java/com/eej/utilities/http/model/MultipleReadHttpServletRequest.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.eej.utilities.http.model;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+
+
+/**
+ * @author DOIBALMI
+ *
+ */
+public class MultipleReadHttpServletRequest extends HttpServletRequestWrapper {
+	
+	private Logger logger = Logger.getLogger(this.getClass()); 
+	
+	private ByteArrayOutputStream cachedBytes;
+
+	public MultipleReadHttpServletRequest(HttpServletRequest request) {
+		super(request);
+	}
+
+	@Override
+	public ServletInputStream getInputStream() throws IOException {
+		if (cachedBytes == null){
+			cacheInputStream();
+		}
+		logger.debug("cachedInputStram.size = " + this.cachedBytes.size());
+		return new CachedServletInputStream();
+	}
+
+	@Override
+	public BufferedReader getReader() throws IOException{
+		return new BufferedReader(new InputStreamReader(getInputStream()));
+	}
+
+	private void cacheInputStream() throws IOException {
+		/* Cache the inputstream in order to read it multiple times. For
+		 * convenience, I use apache.commons IOUtils
+		 */
+		logger.debug("called cacheInputStream()");
+		cachedBytes = new ByteArrayOutputStream();
+		IOUtils.copy(super.getInputStream(), cachedBytes);
+	}
+
+	/* An inputstream which reads the cached request body */
+	public class CachedServletInputStream extends ServletInputStream {
+		private ByteArrayInputStream input;
+
+		public CachedServletInputStream() {
+			/* create a new input stream from the cached request body */
+			input = new ByteArrayInputStream(cachedBytes.toByteArray());
+		}
+
+		@Override
+		public int read() throws IOException {
+			return input.read();
+		}
+	}
+}
